f789f5e73082a5edaeb6bf49ae5886a7e876b0fb
Anna Pozina <anna.pozina@auto1.com>
2024-04-26 08:45:34 +0200

    spike adjusting home work

---
+++ src/Leeson1/Lesson1.java
@@ -0,0 +1,12 @@
+package Leeson1;
+
+public class Lesson1 {
+    public static void main(String[] args) {
+        System.out.print("Anna ");
+        System.out.print("Pozina");
+        System.out.println("");
+        System.out.println("1. Anna");
+        System.out.println("2. Pozina");
+
+    }
+}
